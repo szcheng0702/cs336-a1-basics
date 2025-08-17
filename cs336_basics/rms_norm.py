71e43fd0c654b5def5e6e7340877bf4fe516c671
szcheng0702 <szcheng0702@gmail.com>
2025-08-16 19:35:24 -0700

    fix rms norm

--- cs336_basics/rms_norm.py
+++ cs336_basics/rms_norm.py
@@ -4,19 +4,26 @@ from einops import einsum, reduce
 
 
 class RMSNorm(nn.Module):
-    def __init__(self, d_model: int, eps: float = 1e-5, device=None, dtype=None):
+    def __init__(
+        self,
+        d_model: int,
+        eps: float = 1e-5,
+        device: torch.device | None = None,
+        dtype: torch.dtype | None = None,
+    ):
         super().__init__()
         self.d_model_ = d_model
         self.eps_ = eps
         # initialize weight
-        g = torch.ones(self.d_model_)
+        g = torch.ones(self.d_model_, device=device, dtype=dtype)
         self.weight = nn.Parameter(g)
 
     def forward(self, x: torch.Tensor) -> torch.Tensor:
         in_dtype = x.dtype
         x = x.to(torch.float32)
         squared_norm = reduce(x**2, "... d_model -> ... 1", "sum")
-        rms = torch.sqrt(1 / self.d_model_ * (squared_norm + self.eps_))
+        # or: squared_norm = torch.sqrt(torch.mean(x ** 2, dim=-1, keepdim=True)
+        rms = torch.sqrt(1 / self.d_model_ * squared_norm + self.eps_)
         result = einsum(
             x / rms, self.weight, "... d_model, d_model -> ... d_model"
         )  # element wise
